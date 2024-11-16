56f548ddff2edaf6e771c70e796e9ecce9c2cd74
ErikMazzuco <erikmazzucomatheus789@gmail.com>
2024-11-16 18:13:58 -0300

    erik

--- Sistema.Academia.Modelos/obj/Debug/net8.0/Sistema.Academia.Modelos.AssemblyInfo.cs
+++ Sistema.Academia.Modelos/obj/Debug/net8.0/Sistema.Academia.Modelos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema.Academia.Modelos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b5701338cea5c18c7372f1c5919f70e93a300c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema.Academia.Modelos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema.Academia.Modelos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
